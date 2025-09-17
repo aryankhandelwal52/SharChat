80458537ede75810a81244b74c2d58afeaef8802
Aryan Khandelwal <aryankhandelwal467@gmail.com>
2025-09-18 00:24:05 +0530

    Updated backend and frontend configs, removed unnecessary files

--- backend/src/index.js
+++ backend/src/index.js
@@ -12,13 +12,14 @@ import path from 'path';
 
 const PORT=process.env.PORT;
 const __dirname= path.resolve();
-app.use(express.json());
+app.use(express.json({ limit: "50mb" })); // Increase JSON payload limit
+app.use(express.urlencoded({ limit: "50mb", extended: true })); // For URL-encoded data
 app.use(cookieParser());
 app.use(cors({
-    origin:"http://localhost:5173",
-    credentials:true,
-})
-);
+    origin: "http://localhost:5173",
+    credentials: true,
+}));
+
 
 app.use("/api/auth",authRoutes)
 app.use("/api/messages",messageRoutes);
